67dc48729063bd074bcf23b96298dc380d845bdf
konstantinosporo <konstantinosporo@hotmail.gr>
2024-09-30 20:56:05 +0300

    v0.2-triestoVercelUploadtestsapi

--- api/index.js
+++ api/index.js
@@ -1,1 +1,4 @@
-export default import('../dist/app-angularv18/server/server.mjs').then(module => module.app());
+export default import('../dist/app-angularv18/server/server.mjs').then(module => {
+  console.log("Server function initialized");
+  return module.app();
+});
